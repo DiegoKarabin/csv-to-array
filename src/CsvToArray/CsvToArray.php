fcfad1485ca2d5982c91d6078f0c1a0a1d42d844
Diego Karabin <diegokarabin@gmail.com>
2019-08-23 22:03:26 -0400

    Fixing Issues

--- src/CsvToArray/CsvToArray.php
+++ src/CsvToArray/CsvToArray.php
@@ -4,18 +4,18 @@ namespace CsvToArray;
 
 class CsvToArray
 {
-	public static function remove_double_quotes($str)
+	private static function remove_double_quotes($str)
 	{
 		return str_replace('"', '', $str);
 	}
 
-	public static function split_csv_line_to_array($str_line)
+	private static function split_csv_line_to_array($str_line)
 	{
 		$str_line = str_replace("\r", '', $str_line);
 
 		return array_map(
 			function ($element) {
-				return remove_double_quotes($element);
+				return self::remove_double_quotes($element);
 			},
 			explode('","', $str_line)
 		);
@@ -24,11 +24,11 @@ class CsvToArray
 	public static function parse_strcsv_to_array($str_data)
 	{
 		$array_lines = explode("\n", $str_data);
-		$array_keys = split_csv_line_to_array(array_shift($array_lines));
+                $array_keys = self::split_csv_line_to_array(array_shift($array_lines));
 		$array_data = array();
 
 		foreach ($array_lines as $line) {
-			$array_record_data = split_csv_line_to_array($line);
+			$array_record_data = self::split_csv_line_to_array($line);
 			$array_record = array();
 
 			foreach ($array_keys as $index => $key) {
